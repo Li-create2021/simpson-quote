4d1aefc107bc02cf4ca87b01de998287a90774ed
Liana van Zyl <Lianavz@Lianas-MacBook-Pro.local>
2021-10-30 23:09:25 +0200

    Created Simpson app which fetches quotes from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [simpsonQuote, setSimpsonQuote] = useState(
+    {
+      "quote": "By chilling my loins I increase the chances of impregnating my wife.",
+      "character": "Apu Nahasapeemapetilon",
+      "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+      "characterDirection": "Left"
+    }
+  )
+
+  const getSimpsonQuote = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then((resp) => resp.data)
+      .then((data) => setSimpsonQuote(data[0]))
+      .then(console.log("simpsonQuote: ", simpsonQuote))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='quoteDisplay'>
+        <img src={simpsonQuote.image} alt={simpsonQuote.character} />
+        <p>{simpsonQuote.character}</p>
+        <p>{simpsonQuote.quote}</p>
+      </div>
+      <button
+        type="button"
+        onClick={getSimpsonQuote}>Get Your Quote</button>
     </div>
   );
 }
